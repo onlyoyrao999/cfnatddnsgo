8886ff2ac014aaa72dc291111c6d5d94c960743a
onlyoyrao999 <onlyoyrao@gmail.com>
2026-08-16 21:45:56 +0800

    refactor: remove redundant patch scripts
    Clean up project structure by removing obsolete Python patching utility scripts that were previously used for source code instrumentation.

--- patch_viewmodel_value.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('./app/src/main/java/com/example/ui/MainViewModel.kt', 'r') as f:
-    content = f.read()
-
-content = content.replace("val currentSaved = repository.savedIps.value", "val currentSaved = savedIps.value")
-
-with open('./app/src/main/java/com/example/ui/MainViewModel.kt', 'w') as f:
-    f.write(content)
